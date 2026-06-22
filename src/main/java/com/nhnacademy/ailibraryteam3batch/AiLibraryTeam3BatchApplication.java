97e6c0b10a079de131606a1f76d1f6314230ae01
konayeong <dnjfrhkd402@naver.com>
2026-06-22 11:03:41 +0900

    init

---
+++ src/main/java/com/nhnacademy/ailibraryteam3batch/AiLibraryTeam3BatchApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.ailibraryteam3batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLibraryTeam3BatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiLibraryTeam3BatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/ailibraryteam3batch/AiLibraryTeam3BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.ailibraryteam3batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLibraryTeam3BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
